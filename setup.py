1064e492823c85235907b04eb326d0d74191ba11
Thibault Cohen <titilambert@gmail.com>
2017-12-29 00:16:53 -0500

    Release 2.1.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ install_reqs = parse_requirements('requirements.txt', session=session)
 test_reqs = parse_requirements('test_requirements.txt', session=session)
 
 setup(name='pyfido',
-      version='2.0.1',
+      version='2.1.0',
       description='Get your Fido consumption (wwww.fido.ca)',
       author='Thibault Cohen',
       author_email='titilambert@gmail.com',
